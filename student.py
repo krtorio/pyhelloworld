3e93f657ddce4dc619c7420b9230b24dab9bd93d
Karlo Torio <krtorio@gmail.com>
2018-06-07 17:22:24 +0800

    added student code

---
+++ student.py
@@ -0,0 +1,9 @@
+class Student():
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	@property
+	def get_age_formatted(self):
+		return "%s years old" % self.age
+
---
+++ tests/test_student.py
@@ -0,0 +1,11 @@
+from student import Student
+
+def test_get_student():
+	student = Student('Karlo',37)
+	assert student.get_age_formatted == "37 years old"
+
+
+def test_set_student_age():
+	student = Student('Karlo',37)
+	student.age = 38
+	assert student.get_age_formatted == "38 years old"	
