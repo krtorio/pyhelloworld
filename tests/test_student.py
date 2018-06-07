a381c14544164014ee0d511f44eab672c04eae6e
Karlo Torio <krtorio@gmail.com>
2018-06-07 17:46:56 +0800

    added setup.py script

--- tests/test_student.py
+++ tests/test_student.py
@@ -9,4 +9,4 @@ def test_get_student():
 def test_set_student_age():
 	student = Student('Karlo',37)
 	student.age = 38
-	assert student.get_age_formatted == "38 years old"	
+	assert student.get_age_formatted == "38 years old"
